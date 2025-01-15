7c0eb887024cbf87206164e7dd0b2084958201d9
Baptiste Dechamp <baptiste.dechamp@my-digital-school.org>
2025-01-15 16:38:22 +0100

    Set_Up_BDD-Server_Link_Finish

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const { connectDB } = require('./config/db');
-const film = require('./models/Film');
-const utilisateur = require('./models/utilisateur');
 
-
-// Charger les variables d'environnement
-dotenv.config();
-
-// Connexion à la base de données PostgreSQL
-connectDB();
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-// Middleware pour analyser les données JSON
+// Middleware
 app.use(express.json());
 
-// Routes et autres configurations à ajouter...
+// Connecter à la base de données
+connectDB();
 
-// Synchroniser les modèles avec la base de données
-const syncDB = async () => {
-  try {
-    await sequelize.sync({ force: true }); // force: true pour recréer les tables à chaque démarrage
-    console.log('Les modèles ont été synchronisés avec succès');
-  } catch (err) {
-    console.error('Erreur lors de la synchronisation des modèles:', err);
-  }
-};
-
-syncDB();
-
-// Lancer le serveur
-const PORT = process.env.PORT || 5000;
+// Démarrer le serveur
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
+
